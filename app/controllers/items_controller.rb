2dbceb5f42fb7909b7e0faa40dc353d300ba246e
Gary Hammell <garyhammell@Garys-MacBook-Pro.local>
2014-10-28 01:38:38 -0700

    add strong params to item creation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,18 @@
 class ItemsController < ApplicationController
   def create
-    @item = Item.new(params[:item])
+    @item = Item.new(item_params)
+    unless @item.save
+      p @item.errors
+    end
     respond_to do |format|
       format.js
     end
   end
 
   def new
+  end
+
+  def item_params
+    params.require(:item).permit(:category, :volume, :volume_type, :price, :quantity)
   end
 end
