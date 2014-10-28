e74e0a1b113d78a781e23cf56d5ad0d5416866b2
Gary Hammell <garyhammell@Garys-MacBook-Pro.local>
2014-10-28 14:14:11 -0700

    added slideToggle() for Item Form on add_item_button

--- app/assets/javascripts/option.js
+++ app/assets/javascripts/option.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
   var optionsCount = 0
+  bindShowFormEvent('#add_item_button', '.new_item_form_div')
 })
+
+var bindShowFormEvent = function(showButton, form) {
+  $('body').on('click', showButton, function(){
+    $(this).siblings(form).slideToggle()
+  })
+}
