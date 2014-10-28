c5a24925efa9008328674f08e320e22322a533ba
Gary Hammell <garyhammell@Garys-MacBook-Pro.local>
2014-10-28 04:03:27 -0700

    updated header scroll to work on up and down

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
-  headerScrollEnabler('#header', 3)
+  // headerScrollEnabler('#header', 3)
+  var lastScrollTop = 0;
+  $(window).scroll(function(event){
+    var currentTop = $(this).scrollTop();
+    position = $('#header').css('background-position-y').replace('px','')
+    if (currentTop > lastScrollTop){
+      $('#header').css('background-position-y', parseInt(position) - 1 + 'px')
+     } else {
+      $('#header').css('background-position-y', parseInt(position) + 1 + 'px')
+     }
+     lastScrollTop = currentTop;
+  });
 })
 
-var headerScrollEnabler = function(element, change) {
-  window.onscroll = function(){
-    position = $(element).css('background-position-y').replace('px','')
-    $(element).css('background-position-y', parseInt(position) - change + 'px')
-  }
-}
+// var headerScrollEnabler = function(element, change) {
+//   window.onscroll = function(){
+//     position = $(element).css('background-position-y').replace('px','')
+//     $(element).css('background-position-y', parseInt(position) - change + 'px')
+//   }
+// }
