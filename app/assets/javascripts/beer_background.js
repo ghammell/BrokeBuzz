e7dbe553d80a93b493bce93c43e049e0cdeb23c3
Gary Hammell <garyhammell@Garys-MacBook-Pro.local>
2014-11-01 20:36:05 -0700

    refine formula for movement of images

--- app/assets/javascripts/beer_background.js
+++ app/assets/javascripts/beer_background.js
@@ -1,23 +1,41 @@
 $(document).ready(function(){
   bgGenerator = new BackgroundGenerator()
-  bgGenerator.positioning('.beer_glass_lg', 'top', 'left')
-  bgGenerator.positioning('.beer_glass_mg', 'bottom', 'right')
-  bgGenerator.positioning('.beer_glass_dg', 'top', 'right')
+  bgGenerator.applyPosition('.beer_glass_lg', 'top', 'left')
+  bgGenerator.applyPosition('.beer_glass_mg', 'bottom', 'right')
+  bgGenerator.applyPosition('.beer_glass_dg', 'top', 'right')
+  bgGenerator.applyPosition('.beer_glass_w', 'bottom', 'left')
 })
 
 var BackgroundGenerator = function() {
-  this.positioning = function(selector, directionOne, directionTwo){
-    var top = Math.floor(Math.random() * 200)
-    var left = Math.floor(Math.random() * 200)
+  this.applyPosition = function(selector, topOrBottom, leftOrRight){
+    var dir1 = Math.floor(Math.random() * 200)
+    var dir2 = Math.floor(Math.random() * 200)
     for (i=0; i < $(selector).length; i++) {
+      bindScroll($(selector)[i], topOrBottom)
       var height = Math.floor(Math.random() * 150)
       $($(selector)[i]).height(height + 'px')
-      $($(selector)[i]).css(directionOne, top + 'px')
-      $($(selector)[i]).css(directionTwo, left + 'px')
-      var left = left + Math.floor(Math.random() * 200 + 150)
-      var top = top + Math.floor(Math.random() * 200 + 150)
+      $($(selector)[i]).css(topOrBottom, dir1 + 'px')
+      $($(selector)[i]).css(leftOrRight, dir2 + 'px')
+      var dir1 = dir1 + Math.floor(Math.random() * 200 + 150)
+      var dir2 = dir2 + Math.floor(Math.random() * 200 + 150)
     }
   $(selector).show()
   }
+}
+
+var bindScroll = function(object, direction) {
+  var lastScrollTop = 0;
+  $(window).scroll(function(event){
+    var currentTop = $(this).scrollTop();
+    position = $(object).css(direction).replace('px','')
+    movement = Math.floor(Math.random() * 10 + 2)
+    if (currentTop > lastScrollTop){
+      $(object).css(direction, parseInt(position) - movement + 'px')
+      console.log('moving down')
+    } else {
+      $(object).css(direction, parseInt(position) + movement + 'px')
+    }
+    lastScrollTop = currentTop;
+  });
 }
 
