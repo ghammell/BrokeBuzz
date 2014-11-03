1177da64de655dc703ba16d746c30b9be5fccc9c
Gary Hammell <garyhammell@Garys-MacBook-Pro.local>
2014-11-03 11:57:21 -0800

    push option_id into options session upon creation

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,10 +1,12 @@
 class OptionsController < ApplicationController
   def create
     @option = Option.create
+    session[:options] << @option.id
   end
 
   def destroy
     @option = Option.find(params[:id])
+    session[:options].delete(@option.id)
     @option.destroy
   end
 end
