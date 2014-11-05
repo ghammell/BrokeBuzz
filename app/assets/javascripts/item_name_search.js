eb1b134ec7e3fc9ee7a72e348453605e854a1a65
Gary Hammell <garyhammell@Garys-MacBook-Pro.local>
2014-11-05 12:54:15 -0800

    add "type" into data that is passed back to options controller

--- app/assets/javascripts/item_name_search.js
+++ app/assets/javascripts/item_name_search.js
@@ -7,10 +7,11 @@ var bindNameSearchEvent = function() {
     e.preventDefault()
     var url = $(this).parents('.new_item').attr('action') + '/name_search'
     var search_term = $(this).siblings('input').val()
+    var type = $(this).parents().find('#item_category').val()
     $.ajax({
       url: url,
       method: 'GET',
-      data: {search_term: search_term}
+      data: {search_term: search_term, type: type}
     })
     .done(function(){
       console.log('success')
