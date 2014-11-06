0a3de6c63fb3e911aa15c6b4d9abd55eba08b3d6
Gary Hammell <garyhammell@Garys-MacBook-Pro.local>
2014-11-05 23:13:36 -0800

    add "WineDB" helper class to items_helper.rb

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -8,8 +8,23 @@ module ItemsHelper
     end
 
     def beers_search(search_term)
-      beer_results = self.class.get('/search?q=' + search_term.gsub(" ", "%20") + '&type=beer&key=' + @beer_key)
+      beer_results = self.class.get('/search?q=' + search_term + '&type=beer&key=' + @beer_key)
       parsed_results = JSON.parse(beer_results.body)['data']
+    end
+  end
+
+  class WineDB
+    include HTTParty
+    base_uri 'http://services.wine.com/api/beta2/service.svc/json'
+
+    def initialize
+      @wine_key = '6c3e78bd851428fd73256663f378041d'
+    end
+
+    def wines_search(search_term)
+      wine_results = self.class.get('/catalog?search=' + search_term + '&apikey=' + @wine_key)
+      parsed_results = JSON.parse(wine_results.body)['Products']['List']
+      p parsed_results
     end
   end
 end
