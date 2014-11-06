7479252f843f6fb14767dbb942d2acc0c49d80fb
Gary Hammell <garyhammell@Garys-MacBook-Pro.local>
2014-11-05 23:11:03 -0800

    update "add option" button to change text after first click

--- app/assets/javascripts/option.js
+++ app/assets/javascripts/option.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
   var optionsCount = 0
   bindShowFormEvent('#add_item_button', '.new_item_form_div')
+  bindChangeAddButtonEvent()
 })
 
 var bindShowFormEvent = function(showButton, form) {
   $('body').on('click', showButton, function(){
     $(this).siblings(form).slideToggle()
+  })
+}
+
+var bindChangeAddButtonEvent = function() {
+  $('body').on('click', '#add_option_button', function(){
+    $(this).val('Add Another Option')
   })
 }
