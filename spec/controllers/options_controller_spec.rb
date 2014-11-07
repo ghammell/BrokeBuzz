aa6387d7fbb5a17736de660d8f51b3647207c323
Gary Hammell <ghammell1@gmail.com>
2014-11-07 14:40:09 -0800

    test that the options controller create and destroy methods determine the correct option winner

--- spec/controllers/options_controller_spec.rb
+++ spec/controllers/options_controller_spec.rb
@@ -1,14 +1,27 @@
 require 'spec_helper'
 require 'rails_helper'
 
+
 describe OptionsController do
+  before(:each) do
+    Option.delete_all
+    session[:options] = []
+  end
+
   describe 'POST #create' do
     it 'successfully creates a new option' do
       option_count = Option.count
-      session[:options] = []
       post :create, {:format => 'js'}
 
       expect(Option.count).to eq(option_count + 1)
+    end
+
+    it 'determines the correct option winner' do
+      loser = Option.create(:price_per_oz => 1)
+      session[:options] << loser.id
+
+      post :create, {:format => 'js'}
+      expect(assigns(:option_winner).id).to eq(Option.last.id)
     end
   end
 
@@ -16,11 +29,23 @@ describe OptionsController do
     it 'successfully deletes a given option' do
       assert_raises(ActiveRecord::RecordNotFound) do
         option_id = Option.create.id
-        session[:options] = [option_id]
+        session[:options] << option_id
         delete :destroy, {:id => option_id, :format => 'js'}
 
         Option.find(option_id)
       end
+    end
+
+    it 'determines the correct option winner' do
+      winner = Option.create(:price_per_oz => 1)
+      loser = Option.create(:price_per_oz => 2)
+      deleted = Option.create(:price_per_oz => 0)
+
+      session[:options] = [winner.id, loser.id, deleted.id]
+
+      delete :destroy, {:id => deleted.id, :format => 'js'}
+
+      expect(assigns(:option_winner).id).to eq(winner.id)
     end
   end
 end
