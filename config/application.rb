02bd7be9d8073fbf71e0438ad6caa2af79ddf117
Gary Hammell <ghammell1@gmail.com>
2014-11-09 17:53:49 -0800

    move asset configuration into brokebuzz module

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,5 @@
 require File.expand_path('../boot', __FILE__)
 
-config.serve_static_assets = true
 
 # Pick the frameworks you want:
 require "active_model/railtie"
@@ -17,6 +16,7 @@ Bundler.require(*Rails.groups)
 
 module BrokeBuzz
   class Application < Rails::Application
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
